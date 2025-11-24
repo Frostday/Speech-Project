ef932e7971f1b589ad7af1d7f114e51ac4ceac63
Dhruv Garg <dgarg2@br014.ib.bridges2.psc.edu>
2025-11-24 16:49:45 -0500

    owsm finetuning code

---
+++ owsm_finetuning/run_pred.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -p GPU-shared
+#SBATCH -t 24:00:00
+#SBATCH --gpus=h100-80:1
+#SBATCH -n 2
+#SBATCH --output=output.log
+#SBATCH -e output.err
+
+export HF_HOME=/ocean/projects/cis250086p/dgarg2/hf_cache
+export HUGGINGFACE_HUB_CACHE="$HF_HOME/hub"
+export HF_DATASETS_CACHE="$HF_HOME/datasets"
+export TRANSFORMERS_CACHE="$HF_HOME/transformers"
+mkdir -p "$HUGGINGFACE_HUB_CACHE" "$HF_DATASETS_CACHE" "$TRANSFORMERS_CACHE"
+
+. /ocean/projects/cis250187p/dgarg2/espnet/tools/activate_python.sh && python /ocean/projects/cis250187p/dgarg2/Speech-Project/owsm_finetuning/predict.py
